7771d32378dbff181456a2b64523924cefd32e2a
HoangDSR <54090585+hoang3402@users.noreply.github.com>
2023-04-03 01:35:52 +0700

    Done basic nettruyen (#5)
    * implement getChapters
    
    * implement getChapterDetails
    
    * Implement homepageSectionId 'new_added'
    
    * Implement Search

--- src/Nettruyen/Nettruyen.ts
+++ src/Nettruyen/Nettruyen.ts
@@ -19,7 +19,7 @@ import {
 const DOMAIN = 'https://www.nettruyenvt.com';
 
 export const NettruyenInfo: SourceInfo = {
-    version: '1.0.1',
+    version: '1.0.3',
     name: 'NetTruyen',
     icon: 'icon.jpg',
     author: 'Hoang3409',
@@ -40,7 +40,6 @@ export const NettruyenInfo: SourceInfo = {
 }
 
 export class Nettruyen extends Source {
-
     requestManager = createRequestManager({
         requestsPerSecond: 5,
         requestTimeout: 20000,
@@ -64,7 +63,7 @@ export class Nettruyen extends Source {
     override async getHomePageSections(sectionCallback: (section: HomeSection) => void): Promise<void> {
         let newAdded: HomeSection = createHomeSection({
             id: 'new_added',
-            title: "Truyện Mới Thêm Gần Đây",
+            title: "Truyện Mới Thêm",
             view_more: true,
         });
 
@@ -117,6 +116,7 @@ export class Nettruyen extends Source {
             throw new Error("Error: " + e);
         }
     }
+
     override async getChapters(mangaId: string): Promise<Chapter[]> {
         try {
             const chapters: Chapter[] = [];
@@ -150,43 +150,125 @@ export class Nettruyen extends Source {
             throw new Error("Error: " + e);
         }
     }
+
     override async getChapterDetails(mangaId: string, chapterId: string): Promise<ChapterDetails> {
-        try {
-            const request = createRequestObject({
-                url: DOMAIN,
-                param: chapterId,
-                method: "GET",
-            });
+        const request = createRequestObject({
+            url: DOMAIN,
+            param: chapterId,
+            method: "GET",
+        });
 
-            const data = await this.requestManager.schedule(request, 1);
-            let $ = this.cheerio.load(data.data);
+        const data = await this.requestManager.schedule(request, 1);
+        let $ = this.cheerio.load(data.data);
 
-            const pages: string[] = [];
-            for (let image of $('.page-chapter').toArray()) {
-                var link = $('div.page-chapter > img', image).attr('data-original')!;
-                if (link.indexOf('http') === -1) {//nếu link ko có 'http'
-                    pages.push('http:' + link);
-                } else {
-                    pages.push(link);
-                }
+        const pages: string[] = [];
+        for (let image of $('.page-chapter').toArray()) {
+            var link = $('div.page-chapter > img', image).attr('data-original')!;
+            if (link.indexOf('http') === -1) {//nếu link ko có 'http'
+                pages.push('http:' + link);
+            } else {
+                pages.push(link);
             }
-
-            return createChapterDetails({
-                pages: pages,
-                longStrip: false,
-                id: chapterId,
-                mangaId: mangaId,
-            });
-        } catch (e) {
-            throw new Error("Error: " + e);
         }
+
+        return createChapterDetails({
+            pages: pages,
+            longStrip: false,
+            id: chapterId,
+            mangaId: mangaId,
+        });
     }
+
     override async getSearchResults(query: SearchRequest, metadata: any): Promise<PagedResults> {
-        throw new Error("Method not implemented.");
+        const tiles: MangaTile[] = [];
+
+        const request = createRequestObject({
+            url: `${DOMAIN}/Comic/Services/SuggestSearch.ashx`,
+            param: `?q=${encodeURIComponent(query.title!)}`,
+            method: "GET",
+        });
+
+        let data: Response;
+        try {
+            data = await this.requestManager.schedule(request, 1);
+        } catch (error) {
+            console.log(`searchRequest failed with error: ${error}`);
+            return createPagedResults({
+                results: getServerUnavailableMangaTiles(),
+            });
+        }
+        let $ = this.cheerio.load(data.data);
+
+        for (let item of $('li').toArray()) {
+            tiles.push(createMangaTile({
+                id: $('a', item).attr('href')?.replace(`${DOMAIN}/truyen-tranh/`, '')!,
+                title: createIconText({ text: $('a > h3', item).text() }),
+                image: 'http:' + $('a > img', item).attr('src')!
+            }))
+        }
+
+        if (tiles.length == 0) {
+            tiles.push(createMangaTile({
+                id: '',
+                title: createIconText({ text: $('ul > li').toArray().toString() }),
+                image: ''
+            }));
+            return createPagedResults({
+                results: tiles
+            });
+        }
+
+        return createPagedResults({
+            results: tiles,
+            metadata: metadata,
+        });
+    }
+
+    override async getViewMoreItems(homepageSectionId: string, metadata: any): Promise<PagedResults> {
+        const page: number = metadata?.page ?? 1;
+
+        switch (homepageSectionId) {
+            case 'new_added':
+                break;
+            default:
+                throw new Error('Làm gì có page này?!');
+        }
+
+        const request = createRequestObject({
+            url: `${DOMAIN}/tim-truyen-nang-cao`,
+            param: `?page=${page}`,
+            method: "GET",
+        });
+
+        const data = await this.requestManager.schedule(request, 1);
+        const $ = this.cheerio.load(data.data);
+        const tiles: MangaTile[] = [];
+
+        for (let manga of $('div.item', 'div.row').toArray()) {
+            const title = $('figure.clearfix > figcaption > h3 > a', manga).first().text();
+            const id = $('figure.clearfix > div.image > a', manga).attr('href')?.split('/').pop();
+            const image = $('figure.clearfix > div.image > a > img', manga).first().attr('data-original');
+            const subtitle = $("figure.clearfix > figcaption > ul > li.chapter:nth-of-type(1) > a", manga).last().text().trim();
+            if (!id || !title) continue;
+            tiles.push(createMangaTile({
+                id: id,
+                image: !image ? "https://i.imgur.com/GYUxEX8.png" : 'http:' + image,
+                title: createIconText({ text: title }),
+                subtitleText: createIconText({ text: subtitle }),
+            }));
+        }
+
+        metadata = tiles.length === 0 ? undefined : { page: page + 1 };
+
+        return createPagedResults({
+            results: tiles,
+            metadata: metadata,
+        });
     }
 
     parseNewUpdatedSection($: any): MangaTile[] {
         let newUpdatedItems: MangaTile[] = [];
+
         for (let manga of $('div.item', 'div.row').toArray().splice(0, 10)) {
             const title = $('figure.clearfix > figcaption > h3 > a', manga).first().text();
             const id = $('figure.clearfix > div.image > a', manga).attr('href')?.split('/').pop();
@@ -203,5 +285,9 @@ export class Nettruyen extends Source {
 
         return newUpdatedItems;
     }
+}
+
+function getServerUnavailableMangaTiles(): MangaTile[] {
+    throw new Error("Function not implemented.");
 }
 
