8a3ad4b783aad1c3320ebdfbf66b2a44ce58e9e9
hoang3402 <54090585+hoang3402@users.noreply.github.com>
2023-04-02 15:09:52 +0000

    Update gh-pages to output generated at 10e0185

--- Nettruyen/index.js
+++ Nettruyen/index.js
@@ -367,9 +367,9 @@ __exportStar(require("./SearchFilter"), exports);
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Nettruyen = exports.NettruyenInfo = void 0;
 const paperback_extensions_common_1 = require("paperback-extensions-common");
-const DOMAIN = 'https://www.nettruyenvt.com/';
+const DOMAIN = 'https://www.nettruyenvt.com';
 exports.NettruyenInfo = {
-    version: '1.0.3',
+    version: '1.0.1',
     name: 'NetTruyen',
     icon: 'icon.jpg',
     author: 'Hoang3409',
@@ -413,13 +413,13 @@ class Nettruyen extends paperback_extensions_common_1.Source {
     async getHomePageSections(sectionCallback) {
         let newAdded = createHomeSection({
             id: 'new_added',
-            title: "Truyện Mới Thêm",
+            title: "Truyện Mới Thêm Gần Đây",
             view_more: true,
         });
         //Load empty sections
         sectionCallback(newAdded);
         //New Updates
-        let url = `${DOMAIN}hot`;
+        let url = `${DOMAIN}`;
         let request = createRequestObject({
             url: url,
             method: "GET",
@@ -429,16 +429,96 @@ class Nettruyen extends paperback_extensions_common_1.Source {
         newAdded.items = this.parseNewUpdatedSection($);
         sectionCallback(newAdded);
     }
-    getMangaDetails(mangaId) {
-        throw new Error("Method not implemented.");
+    async getMangaDetails(mangaId) {
+        try {
+            const url = `${DOMAIN}/truyen-tranh/${mangaId}`;
+            const request = createRequestObject({
+                url: url,
+                method: "GET",
+            });
+            const data = await this.requestManager.schedule(request, 1);
+            let $ = this.cheerio.load(data.data);
+            var temp = $('#item-detail > div.detail-info > div > div.col-xs-4.col-image > img');
+            var image = 'http:' + temp.attr('src');
+            var titles = temp.attr('alt');
+            var des = $('#item-detail > div.detail-content > p').text();
+            var id = $('#item-detail > div.detail-info > div > div.col-xs-8.col-info > div.row.rating > div:nth-child(1) > div').attr('data-id');
+            // var rating = $('div.star').attr('data-rating')!;
+            return createManga({
+                id: id,
+                author: "Nettruyen ăn cắp của ai đó",
+                artist: 'chịu á',
+                desc: des,
+                titles: [titles, id],
+                image: image,
+                status: 1,
+                rating: 5,
+                hentai: false,
+                tags: [],
+            });
+        }
+        catch (e) {
+            throw new Error("Error: " + e);
+        }
     }
-    getChapters(mangaId) {
-        throw new Error("Method not implemented.");
+    async getChapters(mangaId) {
+        try {
+            const chapters = [];
+            const request = createRequestObject({
+                url: 'https://www.nettruyenvt.com/Comic/Services/ComicService.asmx/ProcessChapterList',
+                param: `?comicId=${mangaId}`,
+                method: "GET",
+            });
+            const data = await this.requestManager.schedule(request, 1);
+            let list = typeof data.data === "string"
+                ? JSON.parse(data.data)
+                : data.data;
+            for (let chapter of list.chapters) {
+                chapters.push(createChapter({
+                    id: chapter.url,
+                    name: chapter.name,
+                    mangaId: mangaId,
+                    chapNum: Number.parseInt(String(chapter.name).split(' ').at(1)),
+                    langCode: paperback_extensions_common_1.LanguageCode.VIETNAMESE
+                }));
+            }
+            return chapters;
+        }
+        catch (e) {
+            throw new Error("Error: " + e);
+        }
     }
-    getChapterDetails(mangaId, chapterId) {
-        throw new Error("Method not implemented.");
+    async getChapterDetails(mangaId, chapterId) {
+        try {
+            const request = createRequestObject({
+                url: DOMAIN,
+                param: chapterId,
+                method: "GET",
+            });
+            const data = await this.requestManager.schedule(request, 1);
+            let $ = this.cheerio.load(data.data);
+            const pages = [];
+            for (let image of $('.page-chapter').toArray()) {
+                var link = $('div.page-chapter > img', image).attr('data-original');
+                if (link.indexOf('http') === -1) { //nếu link ko có 'http'
+                    pages.push('http:' + link);
+                }
+                else {
+                    pages.push(link);
+                }
+            }
+            return createChapterDetails({
+                pages: pages,
+                longStrip: false,
+                id: chapterId,
+                mangaId: mangaId,
+            });
+        }
+        catch (e) {
+            throw new Error("Error: " + e);
+        }
     }
-    getSearchResults(query, metadata) {
+    async getSearchResults(query, metadata) {
         throw new Error("Method not implemented.");
     }
     parseNewUpdatedSection($) {
@@ -455,48 +535,6 @@ class Nettruyen extends paperback_extensions_common_1.Source {
                 image: !image ? "https://i.imgur.com/GYUxEX8.png" : 'http:' + image,
                 title: createIconText({ text: title }),
                 subtitleText: createIconText({ text: subtitle }),
-            }));
-        }
-        return newUpdatedItems;
-    }
-    async getViewMoreItems(homepageSectionId, metadata) {
-        let page = metadata?.page ?? 1;
-        let param = "";
-        let url = "";
-        switch (homepageSectionId) {
-            case "new_added":
-                param = `?status=-1&sort=15&page=${page}`;
-                url = `${DOMAIN}tim-truyen-nang-cao`;
-                break;
-            default:
-                throw new Error("Requested to getViewMoreItems for a section ID which doesn't exist");
-        }
-        const request = createRequestObject({
-            url,
-            method: 'GET',
-            param,
-        });
-        const response = await this.requestManager.schedule(request, 1);
-        const $ = this.cheerio.load(response.data);
-        const manga = this.parseViewMoreItems($);
-        console.log(manga);
-        return createPagedResults({
-            results: manga,
-            metadata
-        });
-    }
-    parseViewMoreItems($) {
-        let newUpdatedItems = [];
-        for (let item of $('div.row', 'div.item').toArray()) {
-            const title = $('figure.clearfix > figcaption > h3 > a', item).first().text();
-            const id = $('figure.clearfix > div.image > a', item).attr('href')?.split('/').pop();
-            const image = $('figure.clearfix > div.image > a > img', item).first().attr('data-original');
-            if (!id || !title)
-                continue;
-            newUpdatedItems.push(createMangaTile({
-                id: id,
-                image: !image ? "https://i.imgur.com/GYUxEX8.png" : 'http:' + image,
-                title: createIconText({ text: title }),
             }));
         }
         return newUpdatedItems;
