a78bd152d539f21569e077d2879e151e24b19cf2
hoang3402 <54090585+hoang3402@users.noreply.github.com>
2023-04-05 08:27:04 +0000

    Update gh-pages to output generated at 1602d30

--- Nettruyen/source.js
+++ Nettruyen/source.js
@@ -522,18 +522,17 @@ class Nettruyen extends paperback_extensions_common_1.Source {
         });
     }
     async getSearchResults(query, metadata) {
+        let advanced;
         const tiles = [];
         let url = '';
         let param = '';
         if (query.includedTags.length > 0) {
-            let temp = ``;
-            for (let genre of query.includedTags) {
-                temp += `,${genre.id}`;
-            }
+            advanced = true;
             url = `${DOMAIN}/`;
-            param = `?genres=${temp}&notgenres=&gender=-1&status=-1&minchapter=1&sort=0`;
+            param = `?genres=${query.includedTags.map(tag => tag.id).join(',')}&notgenres=&gender=-1&status=-1&minchapter=1&sort=0`;
         }
         else {
+            advanced = false;
             url = `${DOMAIN}/Comic/Services/SuggestSearch.ashx`;
             param = `?q=${encodeURIComponent(query.title)}`;
         }
@@ -553,12 +552,23 @@ class Nettruyen extends paperback_extensions_common_1.Source {
             });
         }
         let $ = this.cheerio.load(data.data);
-        for (let item of $('li').toArray()) {
-            tiles.push(createMangaTile({
-                id: $('a', item).attr('href')?.replace(`${DOMAIN}/truyen-tranh/`, ''),
-                title: createIconText({ text: $('a > h3', item).text() }),
-                image: 'http:' + $('a > img', item).attr('src')
-            }));
+        if (advanced) {
+            for (let item of $('.item').toArray()) {
+                tiles.push(createMangaTile({
+                    id: $('a', item).attr('href')?.replace(`${DOMAIN}/truyen-tranh/`, ''),
+                    title: createIconText({ text: $('h3 > a', item).text() }),
+                    image: 'http:' + $('img', item).attr('data-original')
+                }));
+            }
+        }
+        else {
+            for (let item of $('li').toArray()) {
+                tiles.push(createMangaTile({
+                    id: $('a', item).attr('href')?.replace(`${DOMAIN}/truyen-tranh/`, ''),
+                    title: createIconText({ text: $('a > h3', item).text() }),
+                    image: 'http:' + $('a > img', item).attr('src')
+                }));
+            }
         }
         if (tiles.length == 0) {
             return createPagedResults({
