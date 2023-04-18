0e1dc496966ded309734b4e3a9e9d6ae2fcee31f
hoang3402 <54090585+hoang3402@users.noreply.github.com>
2023-04-18 03:15:51 +0000

    Update gh-pages to output generated at 9edaaa5

--- BaoTangTruyen/source.js
+++ BaoTangTruyen/source.js
@@ -380,17 +380,17 @@ __exportStar(require("./RawData"), exports);
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.convertTime = exports.decodeHtml = exports.BaoTangTruyen = exports.BaoTangTruyenInfo = void 0;
 const paperback_extensions_common_1 = require("paperback-extensions-common");
-const DOMAIN = 'https://baotangtruyen2.com/';
-const userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 14_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Mobile/15E148 Safari/604.1';
+const DOMAIN = "https://baotangtruyen2.com/";
+const userAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 14_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Mobile/15E148 Safari/604.1";
 exports.BaoTangTruyenInfo = {
-    version: '1.0.0',
-    name: 'Bảo Tàng Truyện',
-    icon: 'icon.png',
-    author: 'Hoang3409',
-    description: 'Extension that pulls manga from Bảo Tàng Truyện',
+    version: "1.0.1",
+    name: "Bảo Tàng Truyện",
+    icon: "icon.png",
+    author: "Hoang3409",
+    description: "Extension that pulls manga from Bảo Tàng Truyện",
     contentRating: paperback_extensions_common_1.ContentRating.EVERYONE,
     websiteBaseURL: DOMAIN,
-    authorWebsite: 'https://github.com/hoang3402/extensions-vn',
+    authorWebsite: "https://github.com/hoang3402/extensions-vn",
     language: paperback_extensions_common_1.LanguageCode.VIETNAMESE,
 };
 class BaoTangTruyen extends paperback_extensions_common_1.Source {
@@ -405,7 +405,7 @@ class BaoTangTruyen extends paperback_extensions_common_1.Source {
                         ...(request.headers ?? {}),
                         ...{
                             referer: DOMAIN,
-                            'user-agent': userAgent
+                            "user-agent": userAgent,
                         },
                     };
                     return request;
@@ -413,22 +413,22 @@ class BaoTangTruyen extends paperback_extensions_common_1.Source {
                 interceptResponse: async (response) => {
                     return response;
                 },
-            }
+            },
         });
     }
     async getMangaDetails(mangaId) {
         const request = createRequestObject({
             url: `${DOMAIN}truyen-tranh/a-${mangaId}`,
-            method: "GET"
+            method: "GET",
         });
         const data = await this.requestManager.schedule(request, 1);
         const $ = this.cheerio.load(data.data);
         return createManga({
             id: mangaId,
-            titles: [decodeHtml($('h1.title-detail').text().trim())],
-            desc: decodeHtml($('#summary').text().trim()),
-            image: $('div.col-image > img').attr('data-src'),
-            status: paperback_extensions_common_1.MangaStatus.ONGOING
+            titles: [decodeHtml($("h1.title-detail").text().trim())],
+            desc: decodeHtml($("#summary").text().trim()),
+            image: $("div.col-image > img").attr("data-src"),
+            status: paperback_extensions_common_1.MangaStatus.ONGOING,
         });
     }
     async getChapters(mangaId) {
@@ -436,21 +436,21 @@ class BaoTangTruyen extends paperback_extensions_common_1.Source {
         const request = createRequestObject({
             url: `${DOMAIN}Story/ListChapterByStoryID`,
             param: `?StoryID=${mangaId}`,
-            method: "GET"
+            method: "GET",
         });
         const response = await this.requestManager.schedule(request, 1);
         const $ = this.cheerio.load(response.data);
-        const chapterList = $('li.row').slice(1).toArray();
+        const chapterList = $("li.row").slice(1).toArray();
         var index = chapterList.length;
         for (const item of chapterList) {
-            var time = $('.col-xs-4.text-center.small', item).text();
+            var time = $(".col-xs-4.text-center.small", item).text();
             chapters.push(createChapter({
-                id: $('.chapter > a', item).attr('data-id'),
+                id: $(".chapter > a", item).attr("data-id"),
                 mangaId: mangaId,
-                name: $('.chapter > a', item).text(),
+                name: $(".chapter > a", item).text(),
                 chapNum: index--,
                 langCode: paperback_extensions_common_1.LanguageCode.VIETNAMESE,
-                time: convertTime(decodeHtml(time))
+                time: convertTime(decodeHtml(time)),
             }));
         }
         return chapters;
@@ -458,19 +458,19 @@ class BaoTangTruyen extends paperback_extensions_common_1.Source {
     async getChapterDetails(mangaId, chapterId) {
         const request = createRequestObject({
             url: `${DOMAIN}truyen-tranh/a/b/${chapterId}`,
-            method: "GET"
+            method: "GET",
         });
         const response = await this.requestManager.schedule(request, 1);
         const $ = this.cheerio.load(response.data);
         var pages = [];
-        for (const item of $('img[data-index]').toArray()) {
-            pages.push($(item).attr('src'));
+        for (const item of $("img[data-index]").toArray()) {
+            pages.push($(item).attr("src"));
         }
         return createChapterDetails({
             id: chapterId,
             mangaId: mangaId,
             pages: pages,
-            longStrip: false
+            longStrip: false,
         });
     }
     async getSearchResults(query, metadata) {
@@ -495,16 +495,18 @@ class BaoTangTruyen extends paperback_extensions_common_1.Source {
     }
     parseNewUpdatedSection($) {
         let manga = [];
-        for (const item of $('figure.clearfix').toArray()) {
-            let title = $(item).find('h3 > a').text().trim();
-            let id = $(item).find('a').attr('href').split('-').pop();
-            let image = $(item).find('img').attr('src');
-            let subtitle = $(item).find('ul > li > a').first().text();
+        for (const item of $("figure.clearfix").toArray()) {
+            let title = $(item).find("h3 > a").text().trim();
+            let id = $(item).find("a").attr("href").split("-").pop();
+            let image = $(item).find("img").attr("src");
+            let subtitle = $(item).find("ul > li > a").first().text();
             manga.push(createMangaTile({
                 id: id,
                 image: image,
                 title: createIconText({ text: decodeHtml(title) }),
-                subtitleText: createIconText({ text: decodeHtml(subtitle) }),
+                subtitleText: createIconText({
+                    text: decodeHtml(subtitle),
+                }),
             }));
         }
         return manga;
@@ -519,29 +521,50 @@ class BaoTangTruyen extends paperback_extensions_common_1.Source {
         const request = createRequestObject({
             url: `${DOMAIN}home`,
             param: `?page=${page}&typegroup=0`,
-            method: "GET"
+            method: "GET",
         });
         const data = await this.requestManager.schedule(request, 1);
         const $ = this.cheerio.load(data.data);
         let manga = [];
-        for (const item of $('figure.clearfix').toArray()) {
-            let title = $(item).find('h3 > a').text();
-            let id = $(item).find('a').attr('href').split('-').pop();
-            let image = $(item).find('img').attr('src');
-            let subtitle = $(item).find('ul > li > a').first().text();
+        for (const item of $("figure.clearfix").toArray()) {
+            let title = $(item).find("h3 > a").text();
+            let id = $(item).find("a").attr("href").split("-").pop();
+            let image = $(item).find("img").attr("src");
+            let subtitle = $(item).find("ul > li > a").first().text();
             manga.push(createMangaTile({
                 id: id,
                 image: image,
                 title: createIconText({ text: decodeHtml(title) }),
-                subtitleText: createIconText({ text: decodeHtml(subtitle) }),
+                subtitleText: createIconText({
+                    text: decodeHtml(subtitle),
+                }),
             }));
         }
         return createPagedResults({
             results: manga,
             metadata: {
-                page: page + 1
-            }
+                page: page + 1,
+            },
         });
+    }
+    async filterUpdatedManga(mangaUpdatesFoundCallback, time, ids) {
+        const request = createRequestObject({
+            url: `${DOMAIN}tim-truyen`,
+            method: "GET",
+        });
+        const data = await this.requestManager.schedule(request, 1);
+        const $ = this.cheerio.load(data.data);
+        const updatedManga = [];
+        for (const item of $("figure.clearfix").toArray()) {
+            updatedManga.push($(item).find("a").attr('href').split("-").pop());
+        }
+        const results = [];
+        for (const id of ids) {
+            if (updatedManga.includes(id)) {
+                results.push(id);
+            }
+        }
+        mangaUpdatesFoundCallback(createMangaUpdates({ ids: results }));
     }
 }
 exports.BaoTangTruyen = BaoTangTruyen;
@@ -551,12 +574,12 @@ function decodeHtml(encodedString) {
         "&lt;": "<",
         "&gt;": ">",
         "&amp;": "&",
-        "&quot;": "\"",
+        "&quot;": '"',
         "&apos;": "'",
         "&#39;": "'",
         "&#x2F;": "/",
         "&#x3D;": "=",
-        "&#x22;": "\"",
+        "&#x22;": '"',
         "&#x3C;": "<",
         "&#x3E;": ">",
     };
@@ -579,50 +602,50 @@ exports.decodeHtml = decodeHtml;
 function convertTime(time) {
     var date;
     // 29/12/22
-    if (time.split('/').length == 3) {
-        date = time.split('/');
-        date[2] = '20' + date[2];
+    if (time.split("/").length == 3) {
+        date = time.split("/");
+        date[2] = "20" + date[2];
         return new Date(Number.parseInt(date[2]), Number.parseInt(date[1]) - 1, Number.parseInt(date[0]));
     }
     // 11:44 05/02
-    if (time.includes(':')) {
+    if (time.includes(":")) {
         date = new Date();
-        var temp = time.split(' ');
-        date.setHours(Number.parseInt(temp[0].split(':')[0]));
-        date.setMinutes(Number.parseInt(temp[0].split(':')[1]));
-        date.setDate(Number.parseInt(temp[1].split('/')[0]));
-        date.setMonth(Number.parseInt(temp[1].split('/')[1]) - 1);
+        var temp = time.split(" ");
+        date.setHours(Number.parseInt(temp[0].split(":")[0]));
+        date.setMinutes(Number.parseInt(temp[0].split(":")[1]));
+        date.setDate(Number.parseInt(temp[1].split("/")[0]));
+        date.setMonth(Number.parseInt(temp[1].split("/")[1]) - 1);
         return date;
     }
     // some thing "* trước"
-    if (time.includes('trước')) {
-        var T = Number.parseInt(time.split(' ')[0]);
-        if (time.includes('giây')) {
+    if (time.includes("trước")) {
+        var T = Number.parseInt(time.split(" ")[0]);
+        if (time.includes("giây")) {
             date = new Date();
             date.setSeconds(date.getSeconds() - T);
             return date;
         }
-        if (time.includes('phút')) {
+        if (time.includes("phút")) {
             date = new Date();
             date.setMinutes(date.getMinutes() - T);
             return date;
         }
-        if (time.includes('giờ')) {
+        if (time.includes("giờ")) {
             date = new Date();
             date.setHours(date.getHours() - T);
             return date;
         }
-        if (time.includes('ngày')) {
+        if (time.includes("ngày")) {
             date = new Date();
             date.setDate(date.getDate() - T);
             return date;
         }
-        if (time.includes('tháng')) {
+        if (time.includes("tháng")) {
             date = new Date();
             date.setMonth(date.getMonth() - T);
             return date;
         }
-        if (time.includes('năm')) {
+        if (time.includes("năm")) {
             date = new Date();
             date.setFullYear(date.getFullYear() - T);
             return date;
