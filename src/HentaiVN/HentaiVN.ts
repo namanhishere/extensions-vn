cddbd940ef8c6cd8d15e1d1f0616569c03d71b01
HoangDSR <nvh.02021995@gmail.com>
2023-04-24 21:27:19 +0700

    update!

--- src/HentaiVN/HentaiVN.ts
+++ src/HentaiVN/HentaiVN.ts
@@ -25,7 +25,7 @@ const userAgent =
     'Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1';
 
 export const HentaiVNInfo: SourceInfo = {
-    version: '1.2.3',
+    version: '1.2.4',
     name: 'HentaiVN',
     icon: 'icon.png',
     author: 'Hoang3409',
@@ -35,8 +35,16 @@ export const HentaiVNInfo: SourceInfo = {
     websiteBaseURL: DOMAIN,
     sourceTags: [
         {
+            text: 'Recommended',
+            type: TagType.GREEN,
+        },
+        {
             text: 'Hentai',
             type: TagType.RED,
+        },
+        {
+            text: 'Cloudflare',
+            type: TagType.YELLOW,
         },
     ],
 };
