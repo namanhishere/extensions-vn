ef27689515dd256daa9028d820d941aee11c3920
HoangDSR <nvh.02021995@gmail.com>
2023-05-02 15:25:01 +0700

    Fix img hentaivn

--- src/HentaiVN/HentaiVN.ts
+++ src/HentaiVN/HentaiVN.ts
@@ -25,7 +25,7 @@ const userAgent =
     'Mozilla/5.0 (iPhone; CPU iPhone OS 15_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Mobile/15E148 Safari/604.1';
 
 export const HentaiVNInfo: SourceInfo = {
-    version: '1.2.5',
+    version: '1.2.6',
     name: 'HentaiVN',
     icon: 'icon.png',
     author: 'Hoang3409',
@@ -93,7 +93,7 @@ export class HentaiVN extends Source {
         let $More = this.cheerio.load(moreInfo.data);
 
         let title: string = $('div.page-info > h1 > a').text().trim();
-        let img: string = $('img.cover-1').first().attr('data-cfsrc');
+        let img: string = $('img.cover-1').first().attr('src');
         let author: string = '';
         let status = MangaStatus.ONGOING;
         let views: number = 0;
