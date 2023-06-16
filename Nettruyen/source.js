3390c44ff27793a6af589290d870f97fe917b242
hoang3402 <54090585+hoang3402@users.noreply.github.com>
2023-06-16 08:24:07 +0000

    Update gh-pages to output generated at 28a13c7

--- Nettruyen/source.js
+++ Nettruyen/source.js
@@ -385,9 +385,9 @@ exports.getServerUnavailableMangaTiles = exports.Nettruyen = exports.NettruyenIn
 const paperback_extensions_common_1 = require("paperback-extensions-common");
 const time_1 = require("../utils/time");
 const tags_json_1 = __importDefault(require("./tags.json"));
-const DOMAIN = 'https://m.nettruyenmax.com/';
+const DOMAIN = 'https://www.nettruyenup.vn/';
 exports.NettruyenInfo = {
-    version: '1.2.6',
+    version: '1.2.7',
     name: 'NetTruyen',
     icon: 'icon.jpg',
     author: 'Hoang3409',
