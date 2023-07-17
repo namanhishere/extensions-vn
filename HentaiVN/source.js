84a9a5f92eadf5abff16affd949fdbbfa51b6545
hoang3402 <54090585+hoang3402@users.noreply.github.com>
2023-07-17 07:41:09 +0000

    Update gh-pages to output generated at 80f683c

--- HentaiVN/source.js
+++ HentaiVN/source.js
@@ -1347,7 +1347,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Main = exports.getExportVersion = void 0;
 const time_1 = require("./utils/time");
 const DOMAIN = 'https://animemoiapi.onrender.com/api/';
-const BASE_VERSION = '1.0.0';
+const BASE_VERSION = '1.1.0';
 const getExportVersion = (EXTENSION_VERSION) => {
     return BASE_VERSION.split('.').map((x, index) => Number(x) + Number(EXTENSION_VERSION.split('.')[index])).join('.');
 };
@@ -1540,7 +1540,6 @@ class Main {
             }
         });
         const response = await this.requestManager.schedule(request, 1);
-        console.log(response.data);
         const result = typeof response.data === 'string' ? JSON.parse(response.data) : response.data;
         const tiles = [];
         result.forEach((item) => {
