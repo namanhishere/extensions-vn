0d73eeb92ad91e2690af5d9edda9cd44a8f68b5f
HoangDSR <nvh.02021995@gmail.com>
2023-08-14 17:19:59 +0700

    Add Yuruneko, Fix icon CManga

---
+++ src/Yurineko/Yurineko.ts
@@ -0,0 +1,36 @@
+import {
+    ContentRating,
+    SourceInfo
+} from '@paperback/types'
+import {
+    getExportVersion,
+    Main
+} from '../Main'
+
+const HOST = 'Yurineko'
+import tags from './tags.json'
+
+export const YurinekoInfo: SourceInfo = {
+    description: '',
+    icon: 'icon.png',
+    websiteBaseURL: '',
+    version: getExportVersion('0.0.1'),
+    name: 'Yurineko',
+    language: 'vi',
+    author: 'Hoang3409',
+    contentRating: ContentRating.ADULT
+}
+
+const Domain = 'yurineko.net'
+
+export class Yurineko extends Main {
+    Host = HOST
+    Tags = tags
+
+    HostDomain = `https://${Domain}/`
+    UseId = true
+
+    SearchWithGenres = true
+    SearchWithNotGenres = false
+    SearchWithTitleAndGenre = true
+}
